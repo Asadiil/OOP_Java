d3a334a04b8a504ea0dad1d2fcc1ecd05d148041
Dmitry Dolmatov <dima_dolmatov@mail.ru>
2023-08-01 20:20:38 +0700

    home2

--- home2/Main.java
+++ home2/Main.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Market market = new Market("Зоомагазин классных котиков", 10);
+        Market market = new Market("Магазин", 10);
         market.stock = new ArrayList<>();
         market.stock.add(0, "Клиент1");
         market.stock.add(0, "Клиент2");
         market.stock.add(0, "Клиент3");
         market.greetings();
         market.getIn();
-        market.putPerson("Кавалер кошачьей гвардии");
-        market.giveProduct("Кавалер кошачьей гвардии", market.stock, "Кошка - Тотошка");
+        market.putPerson("Покупатель");
+        market.giveProduct("Покупатель", market.stock, "Клиент1");
         market.getOut();
     }
 }

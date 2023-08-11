ca42f399844033a2fe427cf69e029d8b5368e4f0
Dmitry Dolmatov <dima_dolmatov@mail.ru>
2023-08-11 21:33:26 +0700

    home4

---
+++ home4/Fruit.java
@@ -0,0 +1,12 @@
+public abstract class Fruit {
+    //Поле класса (у каждого фрукта есть вес, но у каждого типа фруктов свой)
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
